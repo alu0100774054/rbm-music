d94fbf0cf93a8da630f8ffeeb19bafca3558a4e5
Erik Zephyr <erikbv99421@gmail.com>
2017-01-10 18:45:35 +0000

    Acabado

--- src/rbm_chords.py
+++ src/rbm_chords.py
@@ -1,5 +1,3 @@
-#This file is heavily based on Daniel Johnson's midi manipulation code in https://github.com/hexahedria/biaxial-rnn-music-composition
-
 import numpy as np
 import pandas as pd
 import msgpack
@@ -9,11 +7,11 @@ from tensorflow.python.ops import control_flow_ops
 from tqdm import tqdm
 
 ###################################################
-# In order for this code to work, you need to place this file in the same 
-# directory as the midi_manipulation.py file and the Pop_Music_Midi directory
-
+# El siguiente archivo es capaz de manipular formato
+# midi.
 import midi_manipulation
 
+# Este metodo obtiene las canciones del directorio gracias a la libreria glob.
 def get_songs(path):
     files = glob.glob('{}/*.mid*'.format(path))
     songs = []
@@ -26,76 +24,95 @@ def get_songs(path):
             raise e           
     return songs
 
-songs = get_songs('Pop_Music_Midi') #These songs have already been converted from midi to msgpack
+#Estas canciones ya estan convertidas de formato midi a msgpack.
+songs = get_songs('Pop_Music_Midi')
 print "{} songs processed".format(len(songs))
-###################################################
 
-### HyperParameters
-# First, let's take a look at the hyperparameters of our model:
+# Red neuronal:
 
-lowest_note = midi_manipulation.lowerBound #the index of the lowest note on the piano roll
-highest_note = midi_manipulation.upperBound #the index of the highest note on the piano roll
-note_range = highest_note-lowest_note #the note range
+# Indice de la nota mas bajas de una pianola.
+lowest_note = midi_manipulation.lowerBound 
 
-num_timesteps  = 15 #This is the number of timesteps that we will create at a time
-n_visible      = 2*note_range*num_timesteps #This is the size of the visible layer. 
-n_hidden       = 50 #This is the size of the hidden layer
+# Indice de la nota mas alta de una pianola.
+highest_note = midi_manipulation.upperBound 
 
-num_epochs = 200 #The number of training epochs that we are going to run. For each epoch we go through the entire data set.
-batch_size = 100 #The number of training examples that we are going to send through the RBM at a time. 
-lr         = tf.constant(0.005, tf.float32) #The learning rate of our model
+# Rango de notas.
+note_range = highest_note-lowest_note 
 
-### Variables:
-# Next, let's look at the variables we're going to use:
+# numero de pasos que genera.
+num_timesteps  = 100 
 
-x  = tf.placeholder(tf.float32, [None, n_visible], name="x") #The placeholder variable that holds our data
-W  = tf.Variable(tf.random_normal([n_visible, n_hidden], 0.01), name="W") #The weight matrix that stores the edge weights
-bh = tf.Variable(tf.zeros([1, n_hidden],  tf.float32, name="bh")) #The bias vector for the hidden layer
-bv = tf.Variable(tf.zeros([1, n_visible],  tf.float32, name="bv")) #The bias vector for the visible layer
+# Tamanio de la capa visible.
+n_visible      = 2*note_range*num_timesteps
 
+# Tamaio de la capa oculta.
+n_hidden       = 50 
 
-#### Helper functions. 
+# El numero de entrenamiento que vamos a correr. Para cada uno examinamos todo el conjunto de datos.
+num_epochs = 200
 
-#This function lets us easily sample from a vector of probabilities
+# El número de ejemplos de entrenamiento que vamos a enviar a través del RBM a la vez.
+batch_size = 100 
+
+# La tasa de aprendizaje de nuestro modelo
+lr         = tf.constant(0.005, tf.float32)
+
+# Variables:
+
+# La variable de marcador de posicion que contiene nuestros datos.
+x  = tf.placeholder(tf.float32, [None, n_visible], name="x")
+
+# La matriz de peso que almacena los pesos de las conexiones.
+W  = tf.Variable(tf.random_normal([n_visible, n_hidden], 0.01), name="W") 
+
+# Bias de la capa oculta. 
+bh = tf.Variable(tf.zeros([1, n_hidden],  tf.float32, name="bh"))
+
+# Bias de la capa visible.
+bv = tf.Variable(tf.zeros([1, n_visible],  tf.float32, name="bv"))
+
+# Devuelve un ejemplo de 0s y 1s en funcion de probabilidades.
 def sample(probs):
-    #Takes in a vector of probabilities, and returns a random vector of 0s and 1s sampled from the input vector
     return tf.floor(probs + tf.random_uniform(tf.shape(probs), 0, 1))
 
-#This function runs the gibbs chain. We will call this function in two places:
-#    - When we define the training update step
-#    - When we sample our music segments from the trained RBM
+# Esta funcion ejecuta la cadena de gibbs. Llamaremos esta funcion en dos lugares:
+# - Cuando definimos el paso de actualizacion del entrenamiento.
+# - Cuando muestreamos nuestros segmentos musicales del RBM entrenado.
 def gibbs_sample(k):
-    #Runs a k-step gibbs chain to sample from the probability distribution of the RBM defined by W, bh, bv
+
+    # Ejecuta una cadena de gibbs k-pasos para muestrear la distribucion de probabilidad de la RBM definida por W, bh, bv
     def gibbs_step(count, k, xk):
-        #Runs a single gibbs step. The visible values are initialized to xk
-        hk = sample(tf.sigmoid(tf.matmul(xk, W) + bh)) #Propagate the visible values to sample the hidden values
-        xk = sample(tf.sigmoid(tf.matmul(hk, tf.transpose(W)) + bv)) #Propagate the hidden values to sample the visible values
+        
+        # Ejecuta un solo paso gibbs. Los valores visibles se inicializan en xk
+        # Propagar los valores visibles para muestrear los valores ocultos
+        hk = sample(tf.sigmoid(tf.matmul(xk, W) + bh)) 
+
+        # Propagar los valores ocultos para muestrear los valores visibles
+        xk = sample(tf.sigmoid(tf.matmul(hk, tf.transpose(W)) + bv)) 
         return count+1, k, xk
 
-    #Run gibbs steps for k iterations
-    ct = tf.constant(0) #counter
+    # contador.
+    ct = tf.constant(0)
     [_, _, x_sample] = control_flow_ops.While(lambda count, num_iter, *args: count < num_iter,
                                          gibbs_step, [ct, tf.constant(k), x], 1, False)
-    #This is not strictly necessary in this implementation, but if you want to adapt this code to use one of TensorFlow's
-    #optimizers, you need this in order to stop tensorflow from propagating gradients back through the gibbs step
     x_sample = tf.stop_gradient(x_sample) 
     return x_sample
 
-### Training Update Code
-# Now we implement the contrastive divergence algorithm. First, we get the samples of x and h from the probability distribution
-#The sample of x
-x_sample = gibbs_sample(1) 
-#The sample of the hidden nodes, starting from the visible state of x
-h = sample(tf.sigmoid(tf.matmul(x, W) + bh)) 
-#The sample of the hidden nodes, starting from the visible state of x_sample
+# ENTRENAMIENTO:
+
+x_sample = gibbs_sample(1)
+h = sample(tf.sigmoid(tf.matmul(x, W) + bh))
 h_sample = sample(tf.sigmoid(tf.matmul(x_sample, W) + bh)) 
 
-#Next, we update the values of W, bh, and bv, based on the difference between the samples that we drew and the original values
+# A continuación, actualizamos los valores de W, bh y bv, 
+# basados en la diferencia entre las muestras que dibujamos 
+# y los valores originales
 size_bt = tf.cast(tf.shape(x)[0], tf.float32)
 W_adder  = tf.mul(lr/size_bt, tf.sub(tf.matmul(tf.transpose(x), h), tf.matmul(tf.transpose(x_sample), h_sample)))
 bv_adder = tf.mul(lr/size_bt, tf.reduce_sum(tf.sub(x, x_sample), 0, True))
 bh_adder = tf.mul(lr/size_bt, tf.reduce_sum(tf.sub(h, h_sample), 0, True))
-#When we do sess.run(updt), TensorFlow will run all 3 update steps
+
+# Ejecuta los 3.
 updt = [W.assign_add(W_adder), bv.assign_add(bv_adder), bh.assign_add(bh_adder)]
 
 
@@ -103,30 +120,32 @@ updt = [W.assign_add(W_adder), bv.assign_add(bv_adder), bh.assign_add(bh_adder)]
 # Now it's time to start a session and run the graph! 
 
 with tf.Session() as sess:
-    #First, we train the model
-    #initialize the variables of the model
+    
+    # Iniciamos el modelo.
     init = tf.initialize_all_variables()
     sess.run(init)
-    #Run through all of the training data num_epochs times
+    
+    # Ejecutar todos los datos de entrenamiento num_epochs veces
     for epoch in tqdm(range(num_epochs)):
         for song in songs:
-            #The songs are stored in a time x notes format. The size of each song is timesteps_in_song x 2*note_range
-            #Here we reshape the songs so that each training example is a vector with num_timesteps x 2*note_range elements
+            
+            # Las canciones se almacenan en un formato de tiempo x notas. El tamaioo de cada cancion es timesteps_in_song x 2 * note_range
+            # Aqui remodelamos las canciones para que cada ejemplo de entrenamiento sea un vector con num_timesteps x 2 * elementos note_range
             song = np.array(song)
             song = song[:np.floor(song.shape[0]/num_timesteps)*num_timesteps]
             song = np.reshape(song, [song.shape[0]/num_timesteps, song.shape[1]*num_timesteps])
-            #Train the RBM on batch_size examples at a time
+            
+            # Entrenar la RBM con ejemplos(tantos como batch_size)
             for i in range(1, len(song), batch_size): 
                 tr_x = song[i:i+batch_size]
                 sess.run(updt, feed_dict={x: tr_x})
 
-    #Now the model is fully trained, so let's make some music! 
-    #Run a gibbs chain where the visible nodes are initialized to 0
     sample = gibbs_sample(1).eval(session=sess, feed_dict={x: np.zeros((10, n_visible))})
     for i in range(sample.shape[0]):
         if not any(sample[i,:]):
             continue
-        #Here we reshape the vector to be time x notes, and then save the vector as a midi file
+        
+        # Aqui vamos a remodelar el vector hora x notas, y luego guardar el vector como un archivo midi
         S = np.reshape(sample[i,:], (num_timesteps, 2*note_range))
         midi_manipulation.noteStateMatrixToMidi(S, "generated_chord_{}".format(i))
             
